c69bbbc78cb741aed1e4a5b808525e15a6b095ee
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2021-05-02 17:54:35 +0000

    Deploying to master from @ melvinpetix/melvinpetix.github.io@c5af6773e04253cfeaa086e60baaf3f2183b7082 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6c4f1424ca727f4eb32d.js"
+    "url": "webpack-runtime-6610469b9b13a000dbb6.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3a3794e045664309a2fbde298aa8f361"
+    "revision": "481d058540c518eb60f2ec63537057e6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
