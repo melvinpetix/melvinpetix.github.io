7ce3ce0306f28a389284a681d8c4973d9216304a
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2021-05-04 23:14:02 +0000

    Deploying to master from @ melvinpetix/melvinpetix.github.io@1f988bcec72a0fadd14bf6b10da7195a323c6036 🚀

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "1bfc9850-235a8846aaec1e7e9c1f.js"
   },
   {
-    "url": "app-a806c831e178253b9f14.js"
+    "url": "app-d405e7b93e11758861e3.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0cbd82e9f72399662bc5a74c5f612b8b"
+    "revision": "45f2647943bb100506f8cc1bfa91cc7d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -50,7 +50,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.json",
-    "revision": "6203a584645bfc62b3ee002effacbe19"
+    "revision": "41f965da9e7341e71705eedbba82e1a6"
   },
   {
     "url": "manifest.webmanifest",
@@ -146,7 +146,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-a806c831e178253b9f14.js`))) {
+  if (!resources || !(await caches.match(`/app-d405e7b93e11758861e3.js`))) {
     return await fetch(event.request)
   }
 
